42b68833c851e4ba3ef5c0a1830d7e343294e9a7
Tri-Phung <triphung15@gmail.com>
2025-10-10 10:10:54 +0700

    update while loop to DeleteElement and InsertElement

--- Module2/src/ss3_array_function/array/bai_tap/delete_element/DeleteElement.java
+++ Module2/src/ss3_array_function/array/bai_tap/delete_element/DeleteElement.java
@@ -1,7 +1,6 @@
 package ss3_array_function.array.bai_tap.delete_element;
 
 import java.util.Scanner;
-import java.util.InputMismatchException;
 
 public class DeleteElement {
     public static void main(String[] args) {
@@ -10,14 +9,15 @@ public class DeleteElement {
             arr[i] = (int) (Math.random() * 100);
         }
         System.out.print("Your array: ");
-        for (int element: arr) {
+        for (int element : arr) {
             System.out.print(element + " ");
         }
         System.out.println();
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the value you want to delete: ");
+        while(true) {
         try {
-            int value = sc.nextInt();
+            System.out.print("Enter the value you want to delete: ");
+            int value = Integer.parseInt(sc.nextLine());
             boolean found = false;
             for (int k : arr) {
                 if (k == value) {
@@ -27,8 +27,7 @@ public class DeleteElement {
             }
             if (!found) {
                 System.out.println("Value not found in array.");
-            }
-            else {
+            } else {
                 for (int i = 0; i < arr.length; i++) {
                     if (arr[i] == value) {
                         for (int j = i; j < arr.length - 1; j++) {
@@ -38,16 +37,13 @@ public class DeleteElement {
                     }
                 }
                 System.out.print("New array: ");
-                for (int element: arr) {
+                for (int element : arr) {
                     System.out.print(element + " ");
                 }
             }
-        }
-        catch (InputMismatchException e) {
+            break;
+        } catch (NumberFormatException e) {
             System.out.println("Input should be integer");
-        }
-        finally {
-            sc.close();
-        }
+        } }
     }
 }
--- Module2/src/ss3_array_function/array/bai_tap/insert_element/InsertElement.java
+++ Module2/src/ss3_array_function/array/bai_tap/insert_element/InsertElement.java
@@ -1,76 +1,71 @@
 package ss3_array_function.array.bai_tap.insert_element;
 
 import java.util.Scanner;
-import java.util.InputMismatchException;
 
 public class InsertElement {
     public static void main(String[] args) {
         int[] arr = new int[10];
-        int randomLength= (int) (Math.random() * arr.length);
+        int randomLength = (int) (Math.random() * arr.length);
         for (int i = 0; i < randomLength; i++) {
             arr[i] = (int) (Math.random() * 100);
         }
         System.out.print("Your array: ");
-        for (int element: arr) {
+        for (int element : arr) {
             System.out.print(element + " ");
         }
         System.out.println();
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the value you want to insert: ");
-        try {
-            int value = sc.nextInt();
-            System.out.print("Enter the index you want to insert: ");
-            int index = sc.nextInt();
-            if (index < 0 || index > arr.length-1) {
-                System.out.println("Index is out of range");
-                return;
-            }
-            if (randomLength == arr.length) {
-                System.out.println("There no more space for inserting");
-                return;
-            }
-            if (arr[arr.length-1] !=0 && index == arr.length-1) {
-                System.out.println("There no more space for inserting");
-            }
-            else {
-                if (index == 0) {
-                    for (int i = arr.length-1; i > 0; i--) {
-                        arr[i] = arr[i-1];
-                    }
-                    arr[0] = value;
-                    System.out.print("New array: ");
-                    for (int element: arr) {
-                        System.out.print(element + " ");
-                    }
+        while (true) {
+            try {
+                System.out.print("Enter the value you want to insert: ");
+                int value = Integer.parseInt(sc.nextLine());
+                System.out.print("Enter the index you want to insert: ");
+                int index = Integer.parseInt(sc.nextLine());
+                if (index < 0 || index > arr.length - 1) {
+                    System.out.println("Index is out of range");
+                    return;
                 }
-                else if (arr[index] == 0) {
-                    arr[index] = value;
-                    System.out.print("New array: ");
-                    for (int element: arr) {
-                        System.out.print(element + " ");
-                    }
+                if (randomLength == arr.length) {
+                    System.out.println("There no more space for inserting");
+                    return;
                 }
-                else {
-                    for (int i = 0; i < arr.length; i++) {
-                        if (i == index) {
-                            for (int j = arr.length - 1; j >= index; j--) {
-                                arr[j] = arr[j-1];
-                            }
-                            arr[index] = value;
-                            System.out.print("New array: ");
-                            for (int element: arr) {
-                                System.out.print(element + " ");
+                if (arr[arr.length - 1] != 0 && index == arr.length - 1) {
+                    System.out.println("There no more space for inserting");
+                } else {
+                    if (index == 0) {
+                        for (int i = arr.length - 1; i > 0; i--) {
+                            arr[i] = arr[i - 1];
+                        }
+                        arr[0] = value;
+                        System.out.print("New array: ");
+                        for (int element : arr) {
+                            System.out.print(element + " ");
+                        }
+                    } else if (arr[index] == 0) {
+                        arr[index] = value;
+                        System.out.print("New array: ");
+                        for (int element : arr) {
+                            System.out.print(element + " ");
+                        }
+                    } else {
+                        for (int i = 0; i < arr.length; i++) {
+                            if (i == index) {
+                                for (int j = arr.length - 1; j >= index; j--) {
+                                    arr[j] = arr[j - 1];
+                                }
+                                arr[index] = value;
+                                System.out.print("New array: ");
+                                for (int element : arr) {
+                                    System.out.print(element + " ");
+                                }
                             }
                         }
                     }
                 }
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Input should be integer");
             }
-        }
-        catch (InputMismatchException e) {
-            System.out.println("Input should be integer");
-        }
-        finally {
-            sc.close();
         }
 
     }
