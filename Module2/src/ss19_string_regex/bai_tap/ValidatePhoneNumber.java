aa36c7e977e7b15867704f087271a291ce191f09
Tri-Phung <triphung15@gmail.com>
2025-11-04 16:52:16 +0700

    add ss19 string vs regex hw

--- Module2/src/ss19_string_regex/bai_tap/ValidatePhoneNumber.java
+++ Module2/src/ss19_string_regex/bai_tap/ValidatePhoneNumber.java
@@ -13,7 +13,7 @@ public class ValidatePhoneNumber {
 
     static void main() {
         ValidatePhoneNumber phoneNumberExample = new ValidatePhoneNumber();
-        String[] validPhoneNumber = new String[]{"(84)-(0978489648)","(84)-(0128489648)"};
+        String[] validPhoneNumber = new String[]{"(84)-(0978489648)","(84)-(0128489648)","(84)-(0128680040)"};
         String[] invalidPhoneNumber = new String[]{"(a8)-(22222222)", "(84)-(22b22222)", "(84)-(9978489648)"};
         for (String phoneNumber : validPhoneNumber) {
             boolean isvalid = phoneNumberExample.validate(phoneNumber);
