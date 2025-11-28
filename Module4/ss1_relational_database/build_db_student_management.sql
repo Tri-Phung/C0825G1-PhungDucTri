3d735816dcbde1a7e8010e5d5af1d03256953787
Tri-Phung <triphung15@gmail.com>
2025-11-28 14:16:11 +0700

    update Module4/ss1_relational_database/build_db_student_management.sql

---
+++ Module4/ss1_relational_database/build_db_student_management.sql
@@ -0,0 +1,17 @@
+create database student_management;
+
+use student_management;
+
+create table class (
+	id int not null,
+    class_name varchar(100) not null,
+    primary key(id)
+);
+
+create table teacher (
+	id int not null,
+    teacher_name varchar(100) not null,
+    age int check (age > 0) not null,
+    country varchar(200) not null,
+    primary key(id)
+);
