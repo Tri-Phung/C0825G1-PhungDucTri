f3a109a9f246d78236d8d437d4e6ec3aa6d8c0ef
Tri-Phung <triphung15@gmail.com>
2025-12-03 08:23:07 +0700

    update student_data_query.sql

--- Module4/ss3_db_operations/bai_tap/student_data_query.sql
+++ Module4/ss3_db_operations/bai_tap/student_data_query.sql
@@ -2,12 +2,12 @@ use quanlysinhvien;
 
 -- Hiển thị tất cả các sinh viên có tên bắt đầu bảng ký tự ‘h’
 select student_name
-from student
+from students
 where student_name like 'h%';
 
 -- Hiển thị các thông tin lớp học có thời gian bắt đầu vào tháng 12.
 select class_name, start_date, class_status
-from class
+from classes
 where month(start_date) = 12;
 
 -- Hiển thị tất cả các thông tin môn học có credit trong khoảng từ 3-5.
@@ -16,13 +16,13 @@ from subjects
 where creadit between 3 and 5;
 
 -- Thay đổi mã lớp(ClassID) của sinh viên có tên ‘Hung’ là 2
-update student
+update students
 set class_id = 2
 where student_name = 'Hung';
 
 -- Hiển thị các thông tin: StudentName, SubName, Mark. Dữ liệu sắp xếp theo điểm thi (mark) giảm dần. nếu trùng sắp theo tên tăng dần.
 select s.student_name, sub.sub_name, m.mark
-from student s
-join mark m on s.student_id = m.student_id
+from students s
+join marks m on s.student_id = m.student_id
 join subjects sub on m.sub_id = sub.sub_id
 order by m.mark desc, s.student_name asc;
