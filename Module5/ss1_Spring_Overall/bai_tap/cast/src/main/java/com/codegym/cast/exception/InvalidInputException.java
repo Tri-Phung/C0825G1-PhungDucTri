701248102e83c681f9bc15825e5b861fe4b7856b
Tri-Phung <triphung15@gmail.com>
2025-12-28 22:31:26 +0700

    fix exception handler ss1 CashController & ss2 CalculatorController

--- Module5/ss1_Spring_Overall/bai_tap/cast/src/main/java/com/codegym/cast/exception/InvalidInputException.java
+++
@@ -1,7 +0,0 @@
-package com.codegym.cast.exception;
-
-public class InvalidInputException extends RuntimeException {
-    public InvalidInputException(String message) {
-        super(message);
-    }
-}
