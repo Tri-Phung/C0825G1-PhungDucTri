571f743cd32b06a703fe8148ced41248c4d4f665
Tri-Phung <triphung15@gmail.com>
2025-12-31 01:14:20 +0700

    add ss4_thymeleaf

---
+++ Module5/ss4_thymeleaf/thuc_hanh/customer-management-thymeleaf/src/main/java/com/codegym/customermanagementthymeleaf/entity/Customer.java
@@ -0,0 +1,4 @@
+package com.codegym.customermanagementthymeleaf.entity.service;
+
+public class Customer {
+}
---
+++ Module5/ss4_thymeleaf/thuc_hanh/customer-management-thymeleaf/src/main/java/com/codegym/customermanagementthymeleaf/service/ICustomerService.java
@@ -0,0 +1,4 @@
+package com.codegym.customermanagementthymeleaf.service;
+
+public interface ICustomerService {
+}
