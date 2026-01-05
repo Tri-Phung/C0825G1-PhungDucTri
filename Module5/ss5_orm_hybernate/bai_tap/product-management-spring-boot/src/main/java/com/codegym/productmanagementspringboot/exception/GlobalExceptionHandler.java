3442423c0eac8b7c83826aea2eebb6d6e0a59c1d
Tri-Phung <triphung15@gmail.com>
2026-01-05 08:41:33 +0700

    update exception handling

---
+++ Module5/ss5_orm_hybernate/bai_tap/product-management-spring-boot/src/main/java/com/codegym/productmanagementspringboot/exception/GlobalExceptionHandler.java
@@ -0,0 +1,46 @@
+package com.codegym.productmanagementspringboot.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.NoResultException;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler({
+            BindException.class,
+            MethodArgumentNotValidException.class,
+            HttpMessageNotReadableException.class,
+            MethodArgumentTypeMismatchException.class,
+            IllegalArgumentException.class,
+            NumberFormatException.class
+    })
+    public String handleBadRequest(Exception ex) {
+        ex.printStackTrace();
+        return "error/400";
+    }
+
+    @ExceptionHandler({
+            NoResultException.class,
+            EntityNotFoundException.class,
+            NoHandlerFoundException.class,
+            NoResourceFoundException.class
+    })
+    public String handleNotFound(Exception ex) {
+        ex.printStackTrace();
+        return "error/404";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public String handleServerError(Exception ex) {
+        ex.printStackTrace();
+        return "error/500";
+    }
+}
